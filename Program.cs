6d710d1dc892afafd62d945db9cfc5146ce2b140
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-12-23 16:34:33 +0300

    Event Handler delegate

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +9,13 @@ namespace EventsAndDelegates
     {
         static void Main(string[] args)
         {
-            var video = new Video() { Title = "Video 1"};
-            var videoEncoder = new VideoEncoder();
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+            var messageService = new MessageService(); // subscriber
+
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
 
             videoEncoder.Encode(video);
         }
--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -7,12 +7,31 @@ using System.Threading.Tasks;
 
 namespace EventsAndDelegates
 {
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
     public class VideoEncoder
     {
+        // 1. Define a Delegate
+        // 2. Define an event based on the delegate
+        // 3. Raise the event
+
+        public event EventHandler<VideoEventArgs> VideoEncoded;
         public void Encode(Video video) 
         {
             Console.WriteLine("Encoding Video...");
-            Thread.Sleep(3000); 
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() {  Video = video});
+            }
         }
     }
 }
